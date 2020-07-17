52f9aaf4622b5e3c052b77ab1d190310a5afcfb1
Bahram3110 <maraimov2309@gmail.com>
2020-07-17 13:40:51 +0600

    moi pervyi commit

---
+++ task1.py
@@ -0,0 +1,5 @@
+
+def is_even(number):
+    return True if number % 2 == 0 else False
+
+
---
+++ task5.py
@@ -0,0 +1,23 @@
+name_racer = input('Please enter your name:')
+def choice_to_number(choice):
+    
+    if name_racer == Usain:
+        return 1
+    if name_racer == Me:
+        return 2
+    else:
+        return 3
+
+place_racer = input('Please enter your place:')
+
+def number_to_choise(number):
+    if place_racer == 1:
+        return 'Usain'
+    if place_racer == 2:
+        return 'Me'
+    else:
+        return 'Aybek'
+
+
+
+
